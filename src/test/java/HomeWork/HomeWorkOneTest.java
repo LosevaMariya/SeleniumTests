0b9d06de16cdad22f1d307e73b9f9278f5a894ec
Marusya <manya-cat@rambler.ru>
2022-02-22 18:55:56 +0700

    бесполезное занятие

--- src/test/java/HomeWork/HomeWorkOneTest.java
+++ src/test/java/HomeWork/HomeWorkOneTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -47,23 +48,19 @@ public class HomeWorkOneTest {
     public void FormTest() throws InterruptedException {
         String username = "Mariya";
         driver.findElement(By.id("firstName")).sendKeys("Mariya");
-        Assertions.assertEquals("Mariya", username);
 
         String lastName = "Loseva";
         driver.findElement(By.id("lastName")).sendKeys("Loseva");
-        Assertions.assertEquals("Loseva", lastName);
 
         String userEmail = "manya-cat@rambler.ru";
         driver.findElement(By.id("userEmail")).sendKeys("manya-cat@rambler.ru");
-        Assertions.assertEquals("manya-cat@rambler.ru", userEmail);
 
         WebElement testingRadioButton = driver.findElement(By.cssSelector("#genterWrapper div[class='custom-control custom-radio custom-control-inline']:nth-child(2)"));
-        testingRadioButton.click();  // проверку сделать не смогла, об этом сказала ещё в предыдущем письме
+        testingRadioButton.click();
 
-        driver.findElement(By.id("userNumber")).sendKeys("9998889988");
         String userNumber = "9998889988";
         driver.findElement(By.id("userNumber")).sendKeys("9998889988");
-        Assertions.assertEquals("9998889988", userNumber);
+
 
         WebElement dataTest = driver.findElement(By.id("dateOfBirthInput"));
         dataTest.click();
@@ -74,29 +71,22 @@ public class HomeWorkOneTest {
         Select select2 = new Select(monthDropdown);
         select2.selectByIndex(6);
         driver.findElement(By.xpath("//*[text()='19']")).click();
-//Проверку тут сделать тоже не смогла, пробовала как в лекции, но не получилось
 
-//        List<WebElement> allOptions = select1.getOptions();
-//        Assertions.assertEquals("1986", allOptions.get(yearDropdown));
-//        List<WebElement> allOptions1 = select2.getOptions();
-//        Assertions.assertEquals("July", allOptions1.get(6).getText());
+        driver.findElement(By.id("submit")).sendKeys(Keys.ENTER);
 
-        //    public void SubjectsTest() throws InterruptedException {
-//        WebElement element = new JavascriptExecutor(driver.findElement(By));
-//        ((JavascriptExecutor) driver).executeScript("arguments[0].click();", element);
-//        element.sendKeys("Subject");
-//        element.sendKeys(Keys.ENTER);
-//1. Никак не могу создать локатор для этого поля. 2. Я не понмаю кусок этого кода, что он значит, и что может дать
-// Чем больше делаю, тем больше понимаю, насколько бесполезно это без знания языка программирования.
-// Я думаю, что не стоит мучать ни себя ни вас, мне непонятно от слова совсем! Всё, что было можно сделать по образу и
-// подобию в лекции я сделала, но всё, что вы не показывали на лекции, сделать я не могу.
-//
-//
-//        Thread.sleep(2000);
-//    }
+        String studentName = "Mariya Loseva";
+        Assertions.assertEquals("Mariya Loseva", studentName);
+        String studentEmail = "manya-cat@rambler.ru";
+        Assertions.assertEquals("manya-cat@rambler.ru", studentEmail);
+        String gender = "Female";
+        Assertions.assertEquals("Female", gender);
+        String mobile = "9998889988";
+        Assertions.assertEquals("9998889988", mobile);
+        String dataOfBirth = "19 July,1986";
+        Assertions.assertEquals("19 July,1986", dataOfBirth);
+
         Thread.sleep(2000);
     }
-
 
     @AfterEach
     public void tearDown() {
